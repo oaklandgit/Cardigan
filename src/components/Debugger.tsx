fd6ad649b2ff791c33151a4d8fea3bd9a435522b
Larry Stone <larry.stone@replit.com>
2022-08-17 04:11:15 +0000

    Better positioning algorithm for new elements.

--- src/components/Debugger.tsx
+++ src/components/Debugger.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './Debugger.css';
 import { v4 as uuid } from 'uuid';
 
@@ -14,17 +15,28 @@ export default function Debugger({
   setCurrentBackgroundId
 }) {
 
-  function addCard() {
-    const id = cardList[cardList.length - 1]?.id + 1 || 1;
-    setCardList([...cardList, { id: id, backgroundId: currentBackgroundId }]);
-    setCurrentCardId(id);
-  }
+  // where to place new elements
+  const startPosition = { x: 200, y: 100 };
+  const [nextOffset, setNextOffset] = useState(startPosition);
 
-  function addElement(type: "button" | "field", pos: [number, number, number, number] = [100, 100, 120, 40]) {
+  function addElement(type: "button" | "field", pos: [number, number, number, number] = [nextOffset.x, nextOffset.y, 120, 40]) {
     const cardId = backgroundMode ? currentBackgroundId : currentCardId;
     const id: string = uuid();
     setElementList([...elementList, { id: id, type: type, cardId: cardId, pos: pos }]);
-    // nextOffset();
+
+    setNextOffset({ x: nextOffset.x + 8, y: nextOffset.y + 8 });
+
+    if (nextOffset.x > 300) {
+      setNextOffset(startPosition);
+    }
+
+  }
+
+  function addCard() {
+    //const id = cardList[cardList.length - 1]?.id + 1 || 1;
+    const id = uuid();
+    setCardList([...cardList, { id: id, backgroundId: currentBackgroundId }]);
+    setCurrentCardId(id);
   }
 
   function prevCard() {
@@ -34,17 +46,17 @@ export default function Debugger({
   function nextCard() {
     console.log('next card');
   }
-  
+
   return (
 
     <div id="debuggerbar">
-          <button onClick={addCard}>+ card</button>
-          <button onClick={() => addElement("button")}>+ Button</button>
-          <button onClick={() => addElement("field")}>+ Field</button>
-          <button onClick={prevCard}>&lt; Prev</button>
-          {currentCardId}
-          <button onClick={nextCard}>Next &gt;</button>
-          <button onClick={() => setBackgroundMode(!backgroundMode)}>BG Mode</button>
-        </div>
+      <button onClick={addCard}>+ card</button>
+      <button onClick={() => addElement("button")}>+ Button</button>
+      <button onClick={() => addElement("field")}>+ Field</button>
+      <button onClick={prevCard}>&lt; Prev</button>
+      {currentCardId}
+      <button onClick={nextCard}>Next &gt;</button>
+      <button onClick={() => setBackgroundMode(!backgroundMode)}>BG Mode</button>
+    </div>
   )
 }
