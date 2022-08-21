28dda93af98747fc0626c533008bc4977bd81a8d
Larry Stone <larry.stone@replit.com>
2022-08-21 19:16:42 +0000

    Menu refactoring

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,34 +1,15 @@
 import styles from './Menu.module.css';
 
-export default function Menu({ children }) {
+export default function Menu({ title, isActive, children }) {
 
   return (
 
     <div className={styles.menu}>
-      {children}
+      <div className={`${styles.title} ${isActive && styles.active}`}>{title}</div>
+      <div className={styles.container} style={{ display: !isActive && 'none'}}>
+        {children}
+      </div>
     </div>
 
-    //label, items, active, hide, handleAction
-
-    // <div onMouseLeave={hide} className={`${styles.label} ${active && styles.active}`}>
-
-    //   {label}
-
-    //   {items &&
-    //     <div className={styles.menu} style={{ display: active ? 'block' : 'none' }}>
-    //       {items.map((item: { label: string, section: boolean, shortcut: string, callback: () => void }, index: number) =>
-    //         <div
-    //           onMouseUp={() => handleAction(item.action)}
-    //           key={index}
-    //           className={`${styles.item} ${item.section && styles.section}`}>
-    //           {item.label}
-    //           <span className={styles.shortcut}>{item.shortcut}</span>
-    //         </div>
-    //       )}
-    //     </div>
-    //   }
-
-
-    // </div>
   )
 }
--- src/components/Menubar.tsx
+++ src/components/Menubar.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { menubarItems } from './menubarItems';
-import MenuTitle from './MenuTitle';
+// import MenuTitle from './MenuTitle';
 import Menu from './Menu';
 import MenuItem from './MenuItem';
 import styles from './Menubar.module.css';
@@ -17,32 +17,22 @@ export default function Menubar({ mode, handleAction }) {
     setSelectedMenuIndex(index);
   }
 
-  const hide = (index: number) => {
-    setSelectedMenuIndex(null);
-  }
-
   return (
 
     <div className={`${styles.menubar} ${mode && styles.backgroundMode}`}>
 
-      <MenuTitle title="">
-        <Menu>
-          <MenuItem title="one" />
-          <MenuItem title="two" />
-          <MenuItem title="three" />
+      {menubarItems.map( (menu, i) =>
+        (
+        <div key={i} onMouseLeave={() => setSelectedMenuIndex(null)} onClick={() => toggleMenu(i)}>
+          <Menu title={menu.label} isActive={i === selectedMenuIndex}>
+          {menu.items?.map( (item, j) => 
+            <MenuItem key={j} title={item.label} />
+          )}
         </Menu>
-       </MenuTitle> 
-      
-      <MenuTitle title="File" />
-      <MenuTitle title="Edit" />
+       </div>
+        )
+      )}
 
-      {/*
-      {menubarItems.map((item, index) => (
-        <div key={item.id} onClick={() => toggleMenu(index)}>
-          <Menu hide={hide} active={index === selectedMenuIndex ? true : false} label={item.label} items={item.items} handleAction={handleAction} />
-        </div>
-      ))}
-      */}
     </div>
   )
 }
